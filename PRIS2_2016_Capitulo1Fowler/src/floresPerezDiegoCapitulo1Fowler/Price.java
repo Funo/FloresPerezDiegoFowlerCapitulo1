2c2d743819f449d9355b8240b99b2df0c2d1474a
funo <sabaku.lnx@gmail.com>
2016-04-11 19:00:56 +0200

    Replace Conditional with Polymorphism:

---
+++ PRIS2_2016_Capitulo1Fowler/src/floresPerezDiegoCapitulo1Fowler/Price.java
@@ -0,0 +1,11 @@
+package floresPerezDiegoCapitulo1Fowler;
+
+abstract class Price {
+	abstract int getPriceCode();
+
+	abstract double getCharge(int daysRented);
+
+	int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
+}
